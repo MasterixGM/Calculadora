50a9e72d565b0c1c158064dfdb332e94c12bb907
MasterixGMR <Dragos.diinu@gmail.com>
2023-11-22 11:08:23 +0100

    Cambios en el codigo aportando funcionalidad de manera mas clara y concisa.

--- src/main/java/com/example/calculadora/Controlador.java
+++ src/main/java/com/example/calculadora/Controlador.java
@@ -13,39 +13,41 @@ public class Controlador {
     @FXML
     private TextField pantalla;
     private String operacionPendiente = "";
-    private char[] caracteres = {'/','X','-','+','%','.'};
+    private String caracteres ="/X-+%.";
     private boolean errorCalculo = false;
-    public Controlador() {
+
+    public Controlador() { // Establecemos en el Controlador el motor de Cálculo que vamos a usar para las operaciones.
         ScriptEngineManager manager = new ScriptEngineManager();
-        ScriptEngine engine = manager.getEngineByName("js");
+        ScriptEngine engine = manager.getEngineByName("js"); // En este caso JavaScript.
     }
-    public void error (String mensaje){
+
+    public void error (String mensaje){ //Metodo para mostrar mensaje de error.
         pantalla.setText(mensaje);
         errorCalculo = true;
     }
+
     public void borrarTodo(MouseEvent mouseEvent) {
         pantalla.clear();
-    }
-    public void borrarUltimo(){
+    } // Metodo para borrar la pantalla.
+    public void borrarUltimo(){ // Metodo para borrar el ultimo digito mostrado.
         if (!pantalla.getText().isEmpty()){
             pantalla.setText(pantalla.getText(0, pantalla.getLength()-1));
             errorCalculo = false;
         }
     }
-    public void clickDigito(MouseEvent mouseEvent){
+
+    public void clickDigito(MouseEvent mouseEvent){ // Metodo para mostrar el dígito pulsado por pantalla
         Button boton = (Button) mouseEvent.getSource();
         pantalla.appendText(boton.getText());
         errorCalculo = false;
     }
-    public void cambiarCaracter(MouseEvent mouseEvent){
+
+    public void cambiarCaracter(MouseEvent mouseEvent){ // Metodo para intercambiar caracteres.
         Button button = (Button) mouseEvent.getSource();
         boolean operadorExiste = false;
         if (!pantalla.getText().isEmpty()){
             char ultimoCaracter = pantalla.getText().charAt(pantalla.getLength()-1);
-            for (char c : caracteres) {
-                if (c == ultimoCaracter){
-                    operadorExiste = true;
-                }
+            operadorExiste = caracteres.indexOf(ultimoCaracter) != -1;
             }
             if (operadorExiste){
                 borrarUltimo();
@@ -55,8 +57,8 @@ public class Controlador {
             }
             errorCalculo = false;
         }
-    }
-    public void resultado() {
+
+    public void resultado() { // Metodo para mostrar el resultado.
         String funcion = pantalla.getText().replace("X", "*");
 
         try {
