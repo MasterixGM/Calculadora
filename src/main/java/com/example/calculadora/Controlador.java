792cda492820fae07ac4b23e1eaa320e26b9974a
MasterixGMR <Dragos.diinu@gmail.com>
2023-11-22 11:13:09 +0100

    Simplificacion del Codigo y Resolucion de Errores

--- src/main/java/com/example/calculadora/Controlador.java
+++ src/main/java/com/example/calculadora/Controlador.java
@@ -4,7 +4,6 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
-import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 import net.objecthunter.exp4j.Expression;
 import net.objecthunter.exp4j.ExpressionBuilder;
@@ -12,34 +11,28 @@ import net.objecthunter.exp4j.ValidationResult;
 public class Controlador {
     @FXML
     private TextField pantalla;
-    private String operacionPendiente = "";
-    private String caracteres ="/X-+%.";
-    private boolean errorCalculo = false;
 
     public Controlador() { // Establecemos en el Controlador el motor de Cálculo que vamos a usar para las operaciones.
         ScriptEngineManager manager = new ScriptEngineManager();
-        ScriptEngine engine = manager.getEngineByName("js"); // En este caso JavaScript.
+        manager.getEngineByName("js");
     }
 
     public void error (String mensaje){ //Metodo para mostrar mensaje de error.
         pantalla.setText(mensaje);
-        errorCalculo = true;
     }
 
-    public void borrarTodo(MouseEvent mouseEvent) {
+    public void borrarTodo() {
         pantalla.clear();
     } // Metodo para borrar la pantalla.
     public void borrarUltimo(){ // Metodo para borrar el ultimo digito mostrado.
         if (!pantalla.getText().isEmpty()){
             pantalla.setText(pantalla.getText(0, pantalla.getLength()-1));
-            errorCalculo = false;
         }
     }
 
     public void clickDigito(MouseEvent mouseEvent){ // Metodo para mostrar el dígito pulsado por pantalla
         Button boton = (Button) mouseEvent.getSource();
         pantalla.appendText(boton.getText());
-        errorCalculo = false;
     }
 
     public void cambiarCaracter(MouseEvent mouseEvent){ // Metodo para intercambiar caracteres.
@@ -47,6 +40,7 @@ public class Controlador {
         boolean operadorExiste = false;
         if (!pantalla.getText().isEmpty()){
             char ultimoCaracter = pantalla.getText().charAt(pantalla.getLength()-1);
+            String caracteres = "/X-+%.";
             operadorExiste = caracteres.indexOf(ultimoCaracter) != -1;
             }
             if (operadorExiste){
@@ -55,8 +49,7 @@ public class Controlador {
             } else {
                 pantalla.appendText(button.getText());
             }
-            errorCalculo = false;
-        }
+    }
 
     public void resultado() { // Metodo para mostrar el resultado.
         String funcion = pantalla.getText().replace("X", "*");
@@ -74,7 +67,6 @@ public class Controlador {
                     error("Error");
                 } else {
                     double resultadoFinal = expression.evaluate();
-                    operacionPendiente = String.valueOf(resultadoFinal);
                     pantalla.setText(String.valueOf(resultadoFinal));
                 }
             }
@@ -84,4 +76,5 @@ public class Controlador {
             error("Introduzca Numeros");
         }
     }
+
 }
